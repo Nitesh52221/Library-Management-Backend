8425a352150fdbbe381b723e68dac341c56aa841
Nitesh <niteshtiwari5222@gmail.com>
2022-08-31 16:33:59 +0530

    DataBase Added

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
-
-const { users } = require("./Data/users.json")
-
+const dotenv = require('dotenv');
+const DbConnection = require('./databaseConnection');
 // importing Routes
 const usersRouter = require("./routes/users");
 const booksRouter = require("./routes/books");
 
 
-
+dotenv.config();
 const app = express();
 
-const port = process.env.port || 3000;
+
+
+const port = 3000;
 
 app.use(express.json());
-
+DbConnection();
 
 /**
  * Routes : /
@@ -35,21 +36,11 @@ app.get("/", (req, res) => {
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 
-
-
-
-
-
-
-
 app.all("/", (req, res) => {
     res.status(500).json({
         message: "PAge not found"
     })
 })
-
-
-
 app.get("*", (req, res) => {
     res.status(500).json({
         message: "This Route Not Exsist ."
