0eb0c9ace0135aaa8dc011f6bd390cffcacb5004
niteshtiwari52 <niteshtiwari5222@gmail.com>
2024-02-24 20:06:22 +0530

    Server Deployment

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const DbConnection = require('./databaseConnection');
+const dotenv = require("dotenv");
+const DbConnection = require("./databaseConnection");
 // importing Routes
 const usersRouter = require("./routes/users");
 const booksRouter = require("./routes/books");
 
-
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 DbConnection();
@@ -21,27 +20,26 @@ DbConnection();
  * Parameters: none
  */
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "WELCOME TO THE LIBRARY.",
-    });
+  res.status(200).json({
+    message: "WELCOME TO THE LIBRARY.",
+  });
 });
 
-
-app.use('/users', usersRouter);
-app.use('/books', booksRouter);
-
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 app.all("/", (req, res) => {
-    res.status(500).json({
-        message: "PAge not found"
-    })
-})
+  res.status(500).json({
+    message: "PAge not found",
+  });
+});
 app.get("*", (req, res) => {
-    res.status(500).json({
-        message: "This Route Not Exsist ."
-    })
-})
+  res.status(500).json({
+    message: "This Route Not Exsist .",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`my Node js server started on port ${port}`)
-})
+  console.log(process.env.MONGO_URI);
+  console.log(`my Node js server started on port ${port}`);
+});
